f2c3afdb8385ef87eb045825368b514088f72822
Eduardo Hilário <48762158+eduardohilariodev@users.noreply.github.com>
2025-04-09 12:22:10 -0300

    feat: create movie card

---
+++ src/components/LikeButton.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { HeartIcon } from 'lucide-vue-next'
+
+const props = defineProps<{
+  isActive: boolean
+}>()
+</script>
+
+<template>
+  <button :style="{ color: isActive ? 'black' : 'lightgray' }" class="hover:cursor-pointer">
+    <HeartIcon :fill="isActive ? 'currentColor' : 'none'" />
+  </button>
+</template>
---
+++ src/components/MovieCard.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import type { Movie } from '@/types'
+import LikeButton from '@/components/LikeButton.vue'
+import { Star } from 'lucide-vue-next'
+import { Button } from '@/components/ui/button'
+
+const props = defineProps<{
+  movie: Movie
+}>()
+</script>
+
+<template>
+  <div class="flex h-fit w-fit flex-col rounded-md border-2 border-gray-300">
+    <div class="relative h-64 w-64">
+      <div class="absolute flex size-full flex-col items-center justify-between p-4">
+        <LikeButton class="self-end" :is-active="false" />
+        <span class="text-white">{{ movie.releaseDate }}</span>
+      </div>
+      <img :src="movie.poster_path" :alt="movie.title" class="size-full bg-gray-300 object-cover" />
+    </div>
+    <div class="flex flex-col items-center justify-center">
+      <span class="text-lg font-bold">{{ movie.title }}</span>
+      <span class="text-sm text-gray-500">{{ movie.releaseDate }}</span>
+      <div class="flex gap-4">
+        <div class="flex items-center gap-2">
+          <Star class="text-gray-500" />
+          <span class="text-sm font-bold text-gray-500">{{ movie.rating }}</span>
+        </div>
+        <span class="text-sm text-gray-500">{{ movie.genre }}</span>
+      </div>
+      <span class="text-sm text-gray-500">{{ movie.price }}</span>
+    </div>
+    <Button>Adicionar</Button>
+  </div>
+</template>
