03d7640640bd56e7fa5736cedebf849c5ec2bf05
Eduardo Hilário <48762158+eduardohilariodev@users.noreply.github.com>
2025-04-15 17:59:05 -0300

    style: remove pointer-events-none class from HomeView - Eliminated the pointer-events-none class from the main container in HomeView.vue to allow user interactions. This change enhances the user experience by enabling clickable elements within the grid layout.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -38,7 +38,7 @@ useIntersectionObserver(
 
 <template>
   <div
-    class="pointer-events-none mx-auto grid max-w-screen-xl grid-flow-dense grid-cols-1 gap-4 px-4 py-24 sm:grid-cols-1 sm:gap-6 sm:px-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+    class="mx-auto grid max-w-screen-xl grid-flow-dense grid-cols-1 gap-4 px-4 py-24 sm:grid-cols-1 sm:gap-6 sm:px-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
   >
     <template v-if="isRecommendationsLoading && movies.length === 0">
       <MovieSkeleton v-for="i in 8" :key="i" />
