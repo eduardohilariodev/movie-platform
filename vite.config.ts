65bdcc47d116720debe42865268eb399d45a0d54
Eduardo Hilário <48762158+eduardohilariodev@users.noreply.github.com>
2025-04-14 21:31:17 -0300

    build: add global context to Rollup options - Introduced 'context: globalThis' in Rollup options to ensure compatibility with global scope.

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      // your existing rollup options
+      context: 'globalThis',
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
